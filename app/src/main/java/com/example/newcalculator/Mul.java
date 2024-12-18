f096b84c12fe29100bc39d176827aa57e44c761d
Gopika-Ajeesh <cgopika1234@gmail.com>
2024-12-18 14:19:55 +0530

    partially completed

--- app/src/main/java/com/example/newcalculator/Mul.java
+++ app/src/main/java/com/example/newcalculator/Mul.java
@@ -1,6 +1,12 @@
 package com.example.newcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,40 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Mul extends AppCompatActivity {
-
+    Button b7,b8;
+    EditText et1,et2;
+    TextView tv;
+    int num1,num2,mulip;
+    String getNum1,getNum2,result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mul);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b7=(Button) findViewById(R.id.back2);
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent k=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(k);
+            }
+        });
+        b8=(Button) findViewById(R.id.mult);
+        tv=(TextView) findViewById(R.id.reTodisp);
+        et1=(EditText) findViewById(R.id.numee1);
+        et2=(EditText) findViewById(R.id.numee2);
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getNum1=et1.getText().toString();
+                getNum2=et2.getText().toString();
+                num1=Integer.parseInt(getNum1);
+                num2=Integer.parseInt(getNum2);
+                mulip=num1*num2;
+                result=String.valueOf(mulip);
+                Toast.makeText(getApplicationContext(),result, Toast.LENGTH_SHORT).show();
+                tv.setText(result);
+            }
         });
     }
 }
